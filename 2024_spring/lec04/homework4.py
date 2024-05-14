db6352a90c97b3de065e7b96fc52a64d0cceb357
Hiroon Wijekoon <hiroonwijekoon@gmail.com>
2024-05-14 10:47:41 +0900

    Completed Homework 4

--- 2024_spring/lec04/homework4.py
+++ 2024_spring/lec04/homework4.py
@@ -5,7 +5,10 @@ def list_to_dict(input_list):
     `input_list` is a value, and the corresponding key is the numerical 
     index of that element in `input_list`. 
     '''
-    pass
+    output_dic = {}
+    for i in range(len(input_list)):
+        output_dic[i] = input_list[i]
+    return output_dic
 
 
 
