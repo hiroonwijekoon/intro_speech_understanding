2e56d1182cce185cefd33961e99beda0af9089ac
Hiroon Wijekoon <hiroonwijekoon@gmail.com>
2024-07-29 17:51:49 +0530

    Update homework13.py

--- 2024_spring/lec13/homework13.py
+++ 2024_spring/lec13/homework13.py
@@ -1,29 +1,19 @@
 import bs4, gtts
 
 def extract_stories_from_NPR_text(text):
-    '''
-    Extract a list of stories from the text of the npr.com webpage.
-    
-    @params: 
-    text (string): the text of a webpage
-    
-    @returns:
-    stories (list of tuples of strings): a list of the news stories in the web page.
-      Each story should be a tuple of (title, teaser), where the title and teaser are
-      both strings.  If the story has no teaser, its teaser should be an empty string.
-    '''
-    raise RuntimeError('You need to write this part!')
+    stories = []
+    for div_tag in text.find_all('div', 'story-text'):
+        titletag = div_tag.find('h3', 'title')
+        teasertag = div_tag.find('p', 'teaser')
+        
+        if teasertag != None:
+            stories.append((titletag.text, teasertag.text))
+        else:
+            stories.append((titletag.text, ""))
     return stories
     
 def read_nth_story(stories, n, filename):
-    '''
-    Read the n'th story from a list of stories.
-    
-    @params:
-    stories (list of tuples of strings): a list of the news stories from a web page
-    n (int): the index of the story you want me to read
-    filename (str): filename in which to store the synthesized audio
-
-    Output: None
-    '''
-    raise RuntimeError('You need to write this part!')
+    text = "Here is a list of today's top stories."
+    for n, story in enumerate(stories):
+        text += "Story Number %d: %s.\n"%(n, story[0])
+    gtts.gTTS(text=text, lang="en").save(filename)
